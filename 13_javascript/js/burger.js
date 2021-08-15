54cf63d45de0c538c7749831c3d24f75de7e339a
Ян <ksenia@MacBook-Air-Ksenia.local>
2021-08-15 09:55:02 +0300

    burger active stylesadded

--- 13_javascript/js/burger.js
+++ 13_javascript/js/burger.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#burger').addEventListener('click', function() {
-    document.querySelector('#menu').classList.toggle('burger__list-active')
+    document.querySelector('#menu').classList.toggle('burger__list-active');
+    document.querySelector('#burger').classList.toggle('burger__button-active')
   })
 })
